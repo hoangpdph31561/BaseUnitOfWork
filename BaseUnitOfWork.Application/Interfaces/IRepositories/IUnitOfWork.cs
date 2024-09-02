c00cd6a874ae105c9c89a6bd2bef6bd80a5f80ee
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-09-02 10:56:46 +0700

    update transaction

--- BaseUnitOfWork.Application/Interfaces/IRepositories/IUnitOfWork.cs
+++ BaseUnitOfWork.Application/Interfaces/IRepositories/IUnitOfWork.cs
@@ -1,8 +1,11 @@
 ﻿namespace BaseUnitOfWork.Application.Interfaces.IRepositories
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
         IExampleRepository Example { get; }
         Task SaveAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
     }
 }
--- BaseUnitOfWork.Infrastructure/Repositories/UnitOfWork.cs
+++ BaseUnitOfWork.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using BaseUnitOfWork.Application.Interfaces.IRepositories;
 using BaseUnitOfWork.Infrastructure.Database.AppDbContext;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace BaseUnitOfWork.Infrastructure.Repositories
 {
@@ -7,6 +8,7 @@ namespace BaseUnitOfWork.Infrastructure.Repositories
     {
         private readonly ApplicationDbContext _context;
         public IExampleRepository Example { get; private set; }
+        private IDbContextTransaction? _currentTransaction;
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
@@ -15,6 +17,56 @@ namespace BaseUnitOfWork.Infrastructure.Repositories
         public async Task SaveAsync(CancellationToken cancellationToken = default)
         {
             await _context.SaveChangesAsync(cancellationToken);
+        }
+        public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            if (_currentTransaction != null)
+            {
+                return; // Transaction already started
+            }
+
+            _currentTransaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+        }
+        public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await SaveAsync(cancellationToken);
+                await _currentTransaction.CommitAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                await RollbackTransactionAsync();
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                }
+            }
+        }
+        public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _currentTransaction.RollbackAsync(cancellationToken);
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
         }
     }
 }
